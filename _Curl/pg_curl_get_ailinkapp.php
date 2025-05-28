ec69aea17881bdf4acc286c626ba89a20a0a434f
Chul Ho Kang <solpakan@naver.com>
2025-05-28 17:04:33 +0900

    Add files via upload

--- _Curl/pg_curl_get_ailinkapp.php
+++ _Curl/pg_curl_get_ailinkapp.php
@@ -26,13 +26,15 @@
 		$result = $connect_db->query( $sql1 ); // $result = sql_query( $sql1 );
 		$row = sql_num_rows($result);          // echo "count_:" . $row;
 		if( $row > 0 ) {
+			$day = date("Y-m-d H:i:s", time());
 			if( isset($tabData['data'][$i]['item_cnt']) == '' || $tabData['data'][$i]['item_cnt'] == NULL) $tabData['data'][$i]['item_cnt'] = 0;
 			$sql = " UPDATE {$tkher['table10_pg_curl_table']} SET 
 				item_cnt   = '".$tabData['data'][$i]['item_cnt']."'  , 
 				if_type    = '".$tabData['data'][$i]['if_type']."'  , 
 				if_data    = '".$tabData['data'][$i]['if_data']."'  , 
 				relation_data    = '".$tabData['data'][$i]['popdata_db']."'  , 
-				item_array       = '".$tabData['data'][$i]['item_array']."' 
+				item_array       = '".$tabData['data'][$i]['item_array']."' ,
+				upday       = '".$day."' 
 				WHERE
 						email      = '".$tabData['data'][$i]['email']."' and
 						pg_code    = '".$tabData['data'][$i]['pg_code']."' 
@@ -60,7 +62,7 @@
 			"; 
 			//sys_link ="https://" . $hostnameA . "/t/tkher_program_data_list.php?pg_code=" . $pg_code;
 		}
-		echo "sql:".$sql;
+		//echo "sql:".$sql;
 		$resultA = $connect_db->query( $sql );
         // 롤백
         if (!$resultA) {
