eed7bdefc5e7cf3c6d2542eaa470e50d7ab71b66
Chul Ho Kang <solpakan@naver.com>
2025-10-15 20:08:53 +0900

    Add files via upload

--- logoutT.php
+++ logoutT.php
@@ -9,7 +9,7 @@
 		set_cookie('ck_mb_id', '', 0);
 		set_cookie('ck_auto', '', 0);
 
-		echo "<script>top.location.href=' https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=https://biogplus.com/kapp/'</script>";
+		echo "<script>top.location.href=' https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=".KAPP_URL_."'</script>";
 		//echo "<script>location.href='/t/chatS/'</script>";
 	} else {
 
@@ -22,7 +22,7 @@
 		// 자동로그인 해제 end --------------------------------
 
 		if( isset($_REQUEST['run_pg']) ) $link = $_REQUEST['run_pg'];
-		else $link = "./";
+		else $link = KAPP_URL_;// "./";
 		//goto_url($link);
 		//$url="/";
 		echo "<script>window.open( '$link' , '_top', ''); </script>";
