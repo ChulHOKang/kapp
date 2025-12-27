c3657601b51de7ff746ed2b88f15cc39eb497ce0
Chul Ho Kang <solpakan@naver.com>
2025-12-27 11:41:50 +0900

    Add files via upload

--- include/js/ajax.mb_email.php
+++ include/js/ajax.mb_email.php
@@ -1,14 +1,11 @@
 <?php
 include_once('../../tkher_start_necessary.php');
 include_once(TKHER_PATH_T_.'/include/lib/tkher_register.lib.php');
-//include_once('./_common.php');
-//include_once(G5_LIB_PATH.'/register.lib.php');
 
 $mb_email = trim($_POST['reg_mb_email']);
 $mb_id    = trim($_POST['reg_mb_id']);
 
 set_session('ss_check_mb_email', '');
-//m_("mb_email : ".$mb_email);
 
 if ($msg = empty_mb_email($mb_email)) {
     m_("mb_email--die : ".$mb_email);
